39c382be9797c09ab2e85d0888a549e29ff157a8
RupanGuvi <rupanguvi@gmail.com>
2024-10-09 19:08:35 +0530

    Navbar Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-center items-center">
+        <div className="text-lg font-bold">
+          <Link to={"/"}>Shopify</Link>
         </div>
-    );
+        <div className="relative flex-1 mx-4">
+          <form>
+            <input
+              type="text"
+              placeholder="Search For Products..."
+              className="w-full border py-2 px-4"
+            />
+            <FaSearch className="absolute top-3 right-3 text-red-600" />
+          </form>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link to={"/cart"}>
+            <FaShoppingCart className="text-xl" />
+          </Link>
+          <button className="hidden md:block">SignIn | SignUp</button>
+          <button className="block md:hidden">
+            <FaUser className="text-xl" />
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center space-x-10 py-4 text-md font-serif">
+        <Link to={"/"} className="hover:underline">
+          Home
+        </Link>
+        <Link to={"/shop"} className="hover:underline">
+          Shop
+        </Link>
+        <Link to={"/about"} className="hover:underline">
+          About
+        </Link>
+        <Link to={"/contact"} className="hover:underline">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,74 +1,79 @@
-export const  Categories = [
-    "Electronics",
-     "Fashion",
-     "Home & Kitchen",
-     "Beauty",
-      "Sports",
-      "Automotive"
-  ]
-  
-  export const mockData = [
-  
+export const Categories = [
+  "Electronics",
+  "Fashion",
+  "Home & Kitchen",
+  "Beauty",
+  "Sports",
+  "Automotive",
+];
+
+export const mockData = [
   {
-    id:1,
-  image:"https://m.media-amazon.com/images/I/71mR1c5l70L._AC_UY350_.jpg",
-  name: "Women's Watch",
-  price: 30.60,
+    id: 1,
+    image: "https://m.media-amazon.com/images/I/71mR1c5l70L._AC_UY350_.jpg",
+    name: "Women's Watch",
+    price: 30.6,
   },
   {
-    id:2,
-  image:"https://m.media-amazon.com/images/I/71drVy8MKoL._AC_UF1000,1000_QL80_.jpg",
-  name: "Toy",
-  price: 65.99,
+    id: 2,
+    image:
+      "https://m.media-amazon.com/images/I/71drVy8MKoL._AC_UF1000,1000_QL80_.jpg",
+    name: "Toy",
+    price: 65.99,
   },
   {
-    id:3,
-  image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097",
-  name: "Headphone",
-  price: 29.99,
+    id: 3,
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097",
+    name: "Headphone",
+    price: 29.99,
   },
   {
-    id:4,
-  image:"https://theskinstory.in/cdn/shop/files/1_101b2734-5b84-4ccd-97c3-ea15e483f1f0.jpg?v=1704451340",
-  name: "Shampoo",
-  price: 19.99,
+    id: 4,
+    image:
+      "https://theskinstory.in/cdn/shop/files/1_101b2734-5b84-4ccd-97c3-ea15e483f1f0.jpg?v=1704451340",
+    name: "Shampoo",
+    price: 19.99,
   },
   {
-    id:5,
-  image:"https://m.media-amazon.com/images/I/81ZUL5rCfmL._AC_UY1100_.jpg",
-  name: "Men's Shirt",
-  price: 49.99,
+    id: 5,
+    image: "https://m.media-amazon.com/images/I/81ZUL5rCfmL._AC_UY1100_.jpg",
+    name: "Men's Shirt",
+    price: 49.99,
   },
   {
-    id:6,
-  image:"https://m.media-amazon.com/images/I/51noMsI8n0L._AC_UY1100_.jpg",
-  name: "Cap",
-  price: 40.99,
+    id: 6,
+    image: "https://m.media-amazon.com/images/I/51noMsI8n0L._AC_UY1100_.jpg",
+    name: "Cap",
+    price: 40.99,
   },
   {
-    id:7,
-  image:"https://rukminim2.flixcart.com/image/2700/2700/l2hwwi80/shoe/t/3/o/5-bab774-shoe-island-white-original-imagdtts8wfm3tyh.jpeg?q=60&crop=false",
-  name: "Shoe",
-  price: 60.99,
+    id: 7,
+    image:
+      "https://rukminim2.flixcart.com/image/2700/2700/l2hwwi80/shoe/t/3/o/5-bab774-shoe-island-white-original-imagdtts8wfm3tyh.jpeg?q=60&crop=false",
+    name: "Shoe",
+    price: 60.99,
   },
-  
+
   {
-    id:8,
-  image:"https://m.media-amazon.com/images/I/71GP0KE+9PL._AC_UY350_.jpg",
-  name: "Men's Watch",
-  price: 30.60,
+    id: 8,
+    image: "https://m.media-amazon.com/images/I/71GP0KE+9PL._AC_UY350_.jpg",
+    name: "Men's Watch",
+    price: 30.6,
   },
   {
-    id:9,
-  image:"https://egoss.in/cdn/shop/products/DSC_9344.jpg?v=1646828569&width=2000",
-  name: "Sandel",
-  price: 19.99,
+    id: 9,
+    image:
+      "https://egoss.in/cdn/shop/products/DSC_9344.jpg?v=1646828569&width=2000",
+    name: "Sandel",
+    price: 19.99,
   },
-  
+
   {
-    id:10,
-  image:"https://homafy.com/wp-content/uploads/2023/06/Blue-kid-school-bag.jpg",
-  name: "Bag",
-  price: 28.99,
+    id: 10,
+    image:
+      "https://homafy.com/wp-content/uploads/2023/06/Blue-kid-school-bag.jpg",
+    name: "Bag",
+    price: 28.99,
   },
-  ]
+];
